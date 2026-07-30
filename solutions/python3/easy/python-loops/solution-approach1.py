9af1f9478259d293906436e849468c76b1e11ab8
Pallavi-MS-source <pallavims192006@gmail.com>
2026-07-30 16:02:19 +0530

    ✅ Loops [python3] — approach1

---
+++ solutions/python3/easy/python-loops/solution-approach1.py
@@ -0,0 +1,18 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true
+# Problem     Loops
+# Difficulty  Easy
+# Subdomain   Introduction
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-07-30, 04:02 p.m.
+# ──────────────────────────────────────────────────
+
+if __name__ == '__main__':
+    
+    n = int(input())
+    i = 0
+    while i < n:
+        print(i * i)
+        i += 1
